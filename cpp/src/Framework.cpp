efcf843820e9a3c9d09c7723cec19c5b6d90eaf1
Zachary Balda <zacharybalda23@gmail.com>
2017-11-30 01:00:26 -0500

    implement initialize, load, and close

--- cpp/src/Framework.cpp
+++ cpp/src/Framework.cpp
@@ -7,16 +7,178 @@
 
 #include "Framework.h"
 
+#define DEFAULT_SCREEN_WIDTH 640
+#define DEFAULT_SCREEN_HEIGHT 480
+
 //
 // Framework
 //
-Framework::Framework ()
+Framework::Framework (void)
+  : gWindow (NULL)
+  , gRenderer (NULL)
+  , gFont (NULL)
+  , gTimeTextTexture (NULL)
+  , gPromptTextTexture (NULL)
 {
+  this->initialize();
+  this->loadMedia();
 }
 
 //
 // ~Framework
 //
-Framework::~Framework ()
+Framework::~Framework (void)
+{
+  // TODO: delete game
+  // destroy media and close SDL
+  this->close();
+}
+
+//
+// initialize
+//
+bool Framework::initialize (void)
+{
+  // initialization flag
+	bool success = true;
+
+	// initialize SDL
+	if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
+	{
+		printf( "SDL could not initialize! SDL Error: %s\n", SDL_GetError() );
+		success = false;
+	}
+	else
+	{
+		// set texture filtering to linear
+		if( !SDL_SetHint( SDL_HINT_RENDER_SCALE_QUALITY, "1" ) )
+		{
+			printf( "Warning: Linear texture filtering not enabled!" );
+		}
+
+		// create window
+		gWindow = SDL_CreateWindow( "Call of 2D Zombies", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
+		if( gWindow == NULL )
+		{
+			printf( "Window could not be created! SDL Error: %s\n", SDL_GetError() );
+			success = false;
+		}
+		else
+		{
+			// create vsynced renderer for window
+			gRenderer = SDL_CreateRenderer( gWindow, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC );
+			if( gRenderer == NULL )
+			{
+				printf( "Renderer could not be created! SDL Error: %s\n", SDL_GetError() );
+				success = false;
+			}
+			else
+			{
+				// initialize renderer color
+				SDL_SetRenderDrawColor( gRenderer, 0xFF, 0xFF, 0xFF, 0xFF );
+
+				// initialize PNG loading
+				int imgFlags = IMG_INIT_PNG;
+				if( !( IMG_Init( imgFlags ) & imgFlags ) )
+				{
+					printf( "SDL_image could not initialize! SDL_image Error: %s\n", IMG_GetError() );
+					success = false;
+				}
+
+				 // initialize SDL_ttf
+				if( TTF_Init() == -1 )
+				{
+					printf( "SDL_ttf could not initialize! SDL_ttf Error: %s\n", TTF_GetError() );
+					success = false;
+				}
+			}
+		}
+	}
+
+	return success;
+}
+
+//
+// load
+//
+bool Framework::load (void)
+{
+  // loading success flag
+	bool success = true;
+
+	// open the font
+	gFont = TTF_OpenFont( "open-sans/OpenSans-Regular.ttf", 28 );
+	if( gFont == NULL )
+	{
+		printf( "Failed to load font! SDL_ttf Error: %s\n", TTF_GetError() );
+		success = false;
+	}
+	else
+	{
+		// set text color as black
+		SDL_Color textColor = { 0, 0, 0, 255 };
+
+		// load prompt texture
+		if( !gPromptTextTexture.loadFromRenderedText( "Press Enter to Reset Start Time.", textColor, gFont, gRenderer ) )
+		{
+			printf( "Unable to render prompt texture!\n" );
+			success = false;
+		}
+	}
+
+	return success;
+}
+
+//
+// close
+//
+void Framework::close (void)
+{
+  // free loaded images
+	gTimeTextTexture.free();
+	gPromptTextTexture.free();
+
+	// free global font
+	TTF_CloseFont( gFont );
+	gFont = NULL;
+
+	// destroy window
+	SDL_DestroyRenderer( gRenderer );
+	SDL_DestroyWindow( gWindow );
+	gWindow = NULL;
+	gRenderer = NULL;
+
+	// quit SDL subsystems
+	TTF_Quit();
+	IMG_Quit();
+	SDL_Quit();
+}
+
+//
+// game_loop
+//
+void Framework::game_loop (void)
+{
+  
+}
+
+//
+// draw
+//
+void Framework::draw (void)
+{
+}
+
+//
+// new_game
+//
+void Framework::new_game (void)
+{
+}
+
+//
+// restart_game
+//
+void Framework::close (void)
 {
 }
--- cpp/src/Framework.h
+++ cpp/src/Framework.h
@@ -29,6 +29,9 @@
 #include "game/Co2dz_Game_World.h"
 #include "game/Co2dz_Game_Over.h"
 
+// using LTexture
+#include "game_objects/LTexture.h"
+
 class Framework
 {
 public:
@@ -38,11 +41,55 @@ public:
     /// destructor
     ~Framework (void);
 
-    // initialize and load media
+protected:
+    /**
+     * Initialize SDL.
+     */
     bool initialize (void);
+
+    /**
+     * Load media.
+     */
     bool load (void);
 
-    //
+    /**
+     * Free media and close SDL.
+     */
+    void close (void);
+
+    /**
+     * Main game loop.
+     */
+    void game_loop (void);
+
+    /**
+     * Draw rendered game to screen.
+     */
+    void draw (void);
+
+    /**
+     * Start new game.
+     */
+    void new_game (void);
+
+    /**
+     * Restart the game.
+     */
+    void restart_game (void);
+
+private:
+    // the window to render to
+    SDL_Window * gWindow;
+
+    // the window renderer
+    SDL_Renderer * gRenderer;
+
+    // globally used font
+    TTF_Font * gFont;
+
+    //Scene textures
+    LTexture gTimeTextTexture;
+    LTexture gPromptTextTexture;
 };
 
 #endif  //  !defined _FRAMEWORK_H_
@@ -51,154 +98,19 @@ public:
 
 
 
-//Using SDL, SDL_image, SDL_ttf, standard IO, strings, and string streams
-#include <SDL2/SDL.h>
-#include <SDL2/SDL_image.h>
-#include <SDL2/SDL_ttf.h>
-#include <stdio.h>
-#include <string>
-#include <sstream>
-#include <stdio.h>
-#include <string>
-#include <sstream>
-#include "LTexture.h"
 
-//Screen dimension constants
-const int SCREEN_WIDTH = 640;
-const int SCREEN_HEIGHT = 480;
 
-//Starts up SDL and creates window
-bool init();
 
-//Loads media
-bool loadMedia();
 
-//Frees media and shuts down SDL
-void close();
 
-//The window we'll be rendering to
-SDL_Window* gWindow = NULL;
 
-//The window renderer
-SDL_Renderer* gRenderer = NULL;
 
-//Globally used font
-TTF_Font *gFont = NULL;
 
-//Scene textures
-LTexture gTimeTextTexture;
-LTexture gPromptTextTexture;
 
-bool init()
-{
-	//Initialization flag
-	bool success = true;
 
-	//Initialize SDL
-	if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
-	{
-		printf( "SDL could not initialize! SDL Error: %s\n", SDL_GetError() );
-		success = false;
-	}
-	else
-	{
-		//Set texture filtering to linear
-		if( !SDL_SetHint( SDL_HINT_RENDER_SCALE_QUALITY, "1" ) )
-		{
-			printf( "Warning: Linear texture filtering not enabled!" );
-		}
 
-		//Create window
-		gWindow = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
-		if( gWindow == NULL )
-		{
-			printf( "Window could not be created! SDL Error: %s\n", SDL_GetError() );
-			success = false;
-		}
-		else
-		{
-			//Create vsynced renderer for window
-			gRenderer = SDL_CreateRenderer( gWindow, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC );
-			if( gRenderer == NULL )
-			{
-				printf( "Renderer could not be created! SDL Error: %s\n", SDL_GetError() );
-				success = false;
-			}
-			else
-			{
-				//Initialize renderer color
-				SDL_SetRenderDrawColor( gRenderer, 0xFF, 0xFF, 0xFF, 0xFF );
 
-				//Initialize PNG loading
-				int imgFlags = IMG_INIT_PNG;
-				if( !( IMG_Init( imgFlags ) & imgFlags ) )
-				{
-					printf( "SDL_image could not initialize! SDL_image Error: %s\n", IMG_GetError() );
-					success = false;
-				}
 
-				 //Initialize SDL_ttf
-				if( TTF_Init() == -1 )
-				{
-					printf( "SDL_ttf could not initialize! SDL_ttf Error: %s\n", TTF_GetError() );
-					success = false;
-				}
-			}
-		}
-	}
-
-	return success;
-}
-
-bool loadMedia()
-{
-	//Loading success flag
-	bool success = true;
-
-	//Open the font
-	gFont = TTF_OpenFont( "open-sans/OpenSans-Regular.ttf", 28 );
-	if( gFont == NULL )
-	{
-		printf( "Failed to load font! SDL_ttf Error: %s\n", TTF_GetError() );
-		success = false;
-	}
-	else
-	{
-		//Set text color as black
-		SDL_Color textColor = { 0, 0, 0, 255 };
-
-		//Load prompt texture
-		if( !gPromptTextTexture.loadFromRenderedText( "Press Enter to Reset Start Time.", textColor, gFont, gRenderer ) )
-		{
-			printf( "Unable to render prompt texture!\n" );
-			success = false;
-		}
-	}
-
-	return success;
-}
-
-void close()
-{
-	//Free loaded images
-	gTimeTextTexture.free();
-	gPromptTextTexture.free();
-
-	//Free global font
-	TTF_CloseFont( gFont );
-	gFont = NULL;
-
-	//Destroy window
-	SDL_DestroyRenderer( gRenderer );
-	SDL_DestroyWindow( gWindow );
-	gWindow = NULL;
-	gRenderer = NULL;
-
-	//Quit SDL subsystems
-	TTF_Quit();
-	IMG_Quit();
-	SDL_Quit();
-}
 
 int main( int argc, char* args[] )
 {
