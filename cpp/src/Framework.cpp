5b515206385317318a7451ca2277b1ab59f0f360
Zachary Balda <zacharybalda23@gmail.com>
2017-11-30 00:10:14 -0500

    add framework base code

---
+++ cpp/src/Framework.cpp
@@ -0,0 +1,22 @@
+// Honor Pledge:
+//
+// I pledge that I have neither given nor
+// received any help on this assignment.
+//
+// zbalda
+
+#include "Framework.h"
+
+//
+// Framework
+//
+Framework::Framework ()
+{
+}
+
+//
+// ~Framework
+//
+Framework::~Framework ()
+{
+}
---
+++ cpp/src/Framework.h
@@ -0,0 +1,26 @@
+// Honor Pledge:
+//
+// I pledge that I have neither given nor
+// revieved any help on this assignment.
+//
+// zbalda
+
+#ifndef _FRAMEWORK_H_
+#define _FRAMEWORK_H_
+
+#include "game/Game.h"
+#include "game/Game_Menu.h"
+#include "game/Game_World.h"
+#include "game/Game_Over.h"
+
+class Framework
+{
+public:
+    /// default constructor
+    Framework (void);
+
+    /// destructor
+    ~Framework (void);
+};
+
+#endif  //  !defined _FRAMEWORK_H_
