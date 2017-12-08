63654e6c756af0c87bfeff0890d7c8075e28ec78
Zachary Balda <zacharybalda23@gmail.com>
2017-12-08 01:35:48 -0500

    add game object cpp file

---
+++ cpp/src/game_objects/Game_Object.cpp
@@ -0,0 +1,92 @@
+// Honor Pledge:
+//
+// I pledge that I have neither given nor
+// revieved any help on this assignment.
+//
+// zbalda
+
+#include "Game_Object.h"
+
+//
+// Game_Object
+//
+Game_Object::Game_Object (std::vector<Component> components)
+{
+}
+
+//
+// ~Game_Object
+//
+Game_Object::~Game_Object (void)
+{
+}
+
+//
+// update
+//
+void Game_Object::update (Game_World & world, Game_Object & camera)
+{
+}
+
+//
+// send
+//
+void Game_Object::send (int message)
+{
+}
+
+//
+// is_alive
+//
+bool Game_Object::is_alive (void)
+{
+}
+
+//
+// kill
+//
+void Game_Object::kill (void)
+{
+}
+
+//
+// add_component
+//
+void Game_Object::add_component (Component & component)
+{
+}
+
+//
+// remove_component
+//
+void Game_Object::remove_component (size_t index)
+{
+}
+
+//
+// get_x
+//
+int Game_Object::get_x (void)
+{
+}
+
+//
+// get_y
+//
+int Game_Object::get_y (void)
+{
+}
+
+//
+// get_vel_x
+//
+int Game_Object::get_vel_x (void)
+{
+}
+
+//
+// get_vel_y
+//
+int Game_Object::get_vel_y (void)
+{
+}
